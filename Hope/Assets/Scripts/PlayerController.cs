5ed2b8d80626db9042240c0467df44f780689e20
CamillaDahlstroem <camishcd@gmail.com>
2016-10-14 13:48:10 +0200

    Updated assets
    New sprites for everything, and created a bunch of colliders. Now we can
    bump into stuff! Also added the basic pickup functionality - the stars
    disappear when you hit them. :)

--- Hope/Assets/Scripts/PlayerController.cs
+++ Hope/Assets/Scripts/PlayerController.cs
@@ -34,7 +34,7 @@ public class PlayerController : MonoBehaviour {
     void OnTriggerEnter2D(Collider2D other)
     {
         //Check the provided Collider2D parameter other to see if it is tagged "PickUp", if it is...
-        if (other.gameObject.CompareTag("PickUp"))
+        if (other.gameObject.CompareTag("Pickups"))
 
             //... then set the other object we just collided with to inactive.
             other.gameObject.SetActive(false);
