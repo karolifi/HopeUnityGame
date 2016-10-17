b2ce9807009e2cd94c07aa89548fd13fdeff4781
CamillaDahlstroem <camishcd@gmail.com>
2016-10-17 13:15:54 +0200

    Added enemy-gravity
    Enemies now suck the player towards themselves! Also made some prefabs
    and layer adjustments.

--- Hope/Assets/Scripts/PlayerController.cs
+++ Hope/Assets/Scripts/PlayerController.cs
@@ -3,28 +3,24 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-
     public float speed;
     private Rigidbody2D rb2d;
     private int count;
 
-    // Use this for initialization
-    void Start () {
+    void Start () 
+	{
         rb2d = GetComponent<Rigidbody2D>();
         count = 0;
     }
-	
-	// Update is called once per frame
-	void FixedUpdate () {
 
-        //Store the current horizontal input in the float moveHorizontal.
+	void FixedUpdate () 
+	{
         float moveHorizontal = Input.GetAxis("Horizontal");
 
-        //Store the current vertical input in the float moveVertical.
-        //float moveVertical = Input.GetAxis("Vertical");
+        float moveVertical = Input.GetAxis("Vertical");
 
-        //Use the two store floats to create a new Vector2 variable movement.
-        Vector2 movement = new Vector2(moveHorizontal, 0.0f);
+		Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        //Vector2 movement = new Vector2(moveHorizontal, 0.0f); //no vertical movement
 
         //Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
         rb2d.AddForce(movement * speed);
@@ -34,12 +30,15 @@ public class PlayerController : MonoBehaviour {
     void OnTriggerEnter2D(Collider2D other)
     {
         //Check the provided Collider2D parameter other to see if it is tagged "PickUp", if it is...
-        if (other.gameObject.CompareTag("Pickups"))
+		if (other.gameObject.CompareTag ("Pickup")) 
+		{
+			//... then set the other object we just collided with to inactive.
+			other.gameObject.SetActive (false);
 
-            //... then set the other object we just collided with to inactive.
-            other.gameObject.SetActive(false);
-
-        //Add one to the current value of our count variable.
-        count = count + 1;
+			//Add one to the current value of our count variable.
+			//could be a change in the light value?
+			count = count + 1;
+		}
+		//if enemy, then execute... some change in the light
     }
 }
