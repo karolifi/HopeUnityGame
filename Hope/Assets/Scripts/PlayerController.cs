36c54357167ed535b67bed915763ab22f00a3a8b
CamillaDahlstroem <camishcd@gmail.com>
2016-10-22 23:39:48 +0200

    Light responds to collissions!
    The light now gets bigger and smaller when you pick up shards or collide
    with the blobs! Also, inserted some audio sources. Cat sounds ftw~

--- Hope/Assets/Scripts/PlayerController.cs
+++ Hope/Assets/Scripts/PlayerController.cs
@@ -7,38 +7,53 @@ public class PlayerController : MonoBehaviour {
     private Rigidbody2D rb2d;
     private int count;
 
+	public float originalRange;
+	public Light lt;
+
     void Start () 
 	{
         rb2d = GetComponent<Rigidbody2D>();
         count = 0;
+
+		lt = GetComponent<Light>();
+		originalRange = lt.range;
     }
 
 	void FixedUpdate () 
 	{
         float moveHorizontal = Input.GetAxis("Horizontal");
-
         float moveVertical = Input.GetAxis("Vertical");
 
 		Vector2 movement = new Vector2(moveHorizontal, moveVertical);
         //Vector2 movement = new Vector2(moveHorizontal, 0.0f); //no vertical movement
 
-        //Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
         rb2d.AddForce(movement * speed);
     }
 
     //OnTriggerEnter2D is called whenever this object overlaps with a trigger collider.
     void OnTriggerEnter2D(Collider2D other)
     {
-        //Check the provided Collider2D parameter other to see if it is tagged "PickUp", if it is...
 		if (other.gameObject.CompareTag ("Pickup")) 
 		{
-			//... then set the other object we just collided with to inactive.
-			other.gameObject.SetActive (false);
+			other.gameObject.SetActive (false); //removes the object that was picked up
 
-			//Add one to the current value of our count variable.
-			//could be a change in the light value?
-			count = count + 1;
+			lt.range = originalRange + 0.5F;
+			originalRange += 0.5F;
 		}
-		//if enemy, then execute... some change in the light
+
+		if (other.gameObject.CompareTag ("Goal")) 
+		{
+			lt.range = originalRange + 10;
+			originalRange += 10;
+		}
     }
+
+	void OnCollisionEnter2D(Collision2D coll) 
+	{
+		if (coll.gameObject.CompareTag ("Enemy")) 
+		{
+			lt.range = originalRange - 0.5F;
+			originalRange -= 0.5F;
+		}
+	}
 }
